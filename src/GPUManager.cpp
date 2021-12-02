0db020779e0cdb1439a0a979f09110dfad5a3d7c
kayagokalp <kayagokalp@sabanciuniv.edu>
2021-12-03 00:39:52 +0300

    Merger error between server and local copy fixed.

--- src/GPUManager.cpp
+++
@@ -1,9 +0,0 @@
-#include "GPUManager.h"
-
-
-int GPUManager::get_seconds_for_task(Task &task){
-//TODO(KAYA) : determine how long it will take for the queue to become empty for the required job.
-
-}
-
-
